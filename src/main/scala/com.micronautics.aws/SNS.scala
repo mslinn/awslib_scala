27d041d3dd9943118b4af091de0566930024b03c
Mike Slinn <mslinn@micronauticsresearch.com>
2014-01-04 03:06:29 -0800

    Working towards SNS support

--- src/main/scala/com.micronautics.aws/SNS.scala
+++ src/main/scala/com.micronautics.aws/SNS.scala
@@ -5,7 +5,8 @@ import com.amazonaws.services.sns.AmazonSNSClient
 import java.lang.{Exception, String}
 import java.io.InputStream
 import scala.Exception
-import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.sns.model._
+import collection.JavaConverters._
 
 class SNS {
   var sns: AmazonSNSClient = _
@@ -39,5 +40,30 @@ class SNS {
     this()
     awsCredentials = new BasicAWSCredentials(key, secret)
     sns = new AmazonSNSClient(awsCredentials)
+  }
+
+  /** Creates a topic if it does not exist. Topics should contain a string unique to the AWS account, such as the publishing server's domain name
+   * @return Some(ARN of the Topic) or None if error */
+  def findOrCreateTopic(name: String): Option[String] = {
+    try {
+      val ltr: ListTopicsResult = sns.listTopics(new ListTopicsRequest)
+      val maybeTopic = ltr.getTopics.asScala.toList.find(_.getTopicArn.endsWith(":" + name))
+      maybeTopic.map(_.getTopicArn).orElse {
+        val ctr: CreateTopicResult = sns.createTopic(new CreateTopicRequest(name))
+        Some(ctr.getTopicArn)
+      }
+    } catch {
+      case e: Exception =>
+        // TODO set up logging
+        //Logger.warn(e)
+        println(e.toString)
+        None
+    }
+  }
+
+  /** @param protocol is most likely "http" or "https"
+    * @param endpoint is URL that will receive HTTP POST of ConfirmSubscription action */
+  def subscribe(arn: String, protocol: String, endpoint: String) = {
+    sns.subscribe(new SubscribeRequest(arn, protocol, endpoint))
   }
 }
